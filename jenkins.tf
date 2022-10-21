9e12513f98bd406cb438f247bf72c77090147c33
Bryan Cross <bryan@thecrossfamily.org>
2022-10-21 07:38:29 -0500

    Jenkins initial commit

---
+++ jenkins.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "jenkins" {
+  name             = "jenkins"
+  repository       = "https://charts.jenkins.io/"
+  chart            = "jenkins"
+  namespace        = "jenkins"
+  create_namespace = true
+
+  values = [templatefile("./helm/values/jenkins_values.yaml", { password = var.password, hostname = var.hostname })]
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
