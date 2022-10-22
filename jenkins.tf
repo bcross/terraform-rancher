d980ff288d8be8a4e6c42a6e4dda970facca7173
Bryan Cross <bryan@thecrossfamily.org>
2022-10-22 15:23:10 -0500

    Jenkins now depends on gitea

--- jenkins.tf
+++ jenkins.tf
@@ -1,9 +1,9 @@
 resource "helm_release" "jenkins" {
-  name             = "jenkins"
-  repository       = "https://charts.jenkins.io/"
-  chart            = "jenkins"
+  name       = "jenkins"
+  repository = "https://charts.jenkins.io/"
+  chart      = "jenkins"
 
   values = [templatefile("./helm/values/jenkins_values.yaml", { password = var.password, hostname = var.hostname })]
 
-  depends_on = [helm_release.cert_manager]
+  depends_on = [helm_release.gitea]
 }
