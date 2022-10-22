4b09ddd58fd9e5b8e0c5f2a5854909f9743ae455
Bryan Cross <bryan@thecrossfamily.org>
2022-10-22 15:23:46 -0500

    Gitea push rancher

---
+++ gitea_config.tf
@@ -0,0 +1,23 @@
+resource "gitea_repository" "rancher" {
+  name     = "rancher"
+  username = "gitea_admin"
+  private  = true
+  auto_init = false
+}
+
+resource "null_resource" "rancher_git" {
+    provisioner "local-exec" {
+        command = <<EOF
+            cd git/rancher
+            git init
+            git config user.email "no@no.no"
+            git config user.name "gitea_admin"
+            git checkout -b main
+            git add .
+            git commit -m "initial commit"
+            git push http://gitea_admin:${var.password}@${var.hostname}/gitea/gitea_admin/rancher.git --all
+        EOF
+    }
+
+    depends_on = [helm_release.gitea]
+}
