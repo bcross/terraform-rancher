7bda5cb62e57a59d2b379152751b88d0c1b66494
Bryan Cross <bryan@thecrossfamily.org>
2022-10-22 14:51:55 -0500

    Moved stuff to default namespace

--- gitea.tf
+++ gitea.tf
@@ -3,9 +3,6 @@ resource "helm_release" "gitea" {
   repository = "https://dl.gitea.io/charts/"
   chart      = "gitea"
 
-  namespace        = "gitea"
-  create_namespace = true
-
   values = [templatefile("./helm/values/gitea_values.yaml", { password = var.password, hostname = var.hostname })]
 
   depends_on = [helm_release.cert_manager]
--- jenkins.tf
+++ jenkins.tf
@@ -2,8 +2,6 @@ resource "helm_release" "jenkins" {
   name             = "jenkins"
   repository       = "https://charts.jenkins.io/"
   chart            = "jenkins"
-  namespace        = "jenkins"
-  create_namespace = true
 
   values = [templatefile("./helm/values/jenkins_values.yaml", { password = var.password, hostname = var.hostname })]
 
